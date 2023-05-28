3e7d14f7a9f157a5a405d9f485f65440415ad737
ag0711 <ag0711@tamu.edu>
2023-05-28 01:22:23 -0500

    Added in search placeholder function

--- web_search.py
+++ web_search.py
@@ -1,2 +1,6 @@
 # Used to search the internet and scrape answers from the web
 # Empty at the moment
+
+def search(query):
+    # Used to search the internet and scrape answers from the web
+    return
