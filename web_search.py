3c620f3f69665b6f090f04ea7841ffc69f62610b
ag0711 <ag0711@tamu.edu>
2023-05-29 00:56:25 -0500

    Added in get_results functionality

--- web_search.py
+++ web_search.py
@@ -1,6 +1,21 @@
-# Used to search the internet and scrape answers from the web
-# Empty at the moment
+import requests
+import json
+import os
+from dotenv import load_dotenv
+from googleapiclient.discovery import build
 
-def search(query):
-    # Used to search the internet and scrape answers from the web
+# Used to search the internet and scrape answers from the web
+# Using the Google Custom Search JSON API
+
+def debug_load_dotenv():
+    load_dotenv()
     return
+
+def get_results(search_query):
+    # Used to search the internet and get search results from the web
+    debug_load_dotenv()
+    
+    service = build("customsearch", "v1", developerKey = os.getenv("GOOGLE_API_KEY"))
+    res = service.cse().list(q = search_query, cx = os.getenv("GOOGLE_CSE_ID")).execute()
+
+    return res
