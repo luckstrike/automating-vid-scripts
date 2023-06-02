e2910b7090dff018cc52671775b53ef36b156097
ag0711 <ag0711@tamu.edu>
2023-06-02 00:30:54 -0500

    Parallelized chunks in summarize_info for speed

--- openai_gpt.py
+++ openai_gpt.py
@@ -3,6 +3,7 @@ import openai
 import os
 from dotenv import load_dotenv
 from openai.error import OpenAIError
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 def debug_load_dotenv():
     load_dotenv()
@@ -73,34 +74,32 @@ def query_sense():
     # Note: Should this be used? A funtion to check if the web results make sense?
     return
 
-def summarize_info(content):
-    # Currently doesn't contain any sentence bound checking!
-    # Will make the summary weird in some cases (if a word is cut off)
+def ask_gpt_chunk(chunk):
+    print("DEBUG: Chunk has been fed into GPT")
+    try:
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant who summarizes any user provided text."},
+                {"role": "user", "content": chunk}
+            ],
+            temperature=0.5  # creativity factor, from 0 to 1 where one is more creative
+        )
+        return response['choices'][0]['message']['content']
+    except OpenAIError as e:
+        print(f"Uh oh, an API error occurred. {e}")
+        return None
+    except Exception as e:
+        print(f"Uh oh, an error occurred: {e}")
+        return None
 
+def summarize_info(content):
     openai.api_key = os.getenv("OPENAI_API_KEY")  # sets the api key
 
-    # Divide the text into chunks of 2048 tokens each
+    # Divide the text into chunks of 2048 tokens each (no sentence splitting)
     content_chunks = [content[i:i+2048] for i in range(0, len(content), 2048)]
 
-    summary = ""
-    for chunk in content_chunks:
-        print("DEBUG: Chunk has been sent to GPT")
-        # Runs the GPT-3.5-turbo model
-        try:
-            response = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages=[
-                    {"role": "system", "content": "You are a helpful assistant who summarizes any user provided text."},
-                    {"role": "user", "content": chunk}
-                ],
-                temperature=0.5  # creativity factor, from 0 to 1 where one is more creative
-            )
-            summary += response['choices'][0]['message']['content']
-        except OpenAIError as e:
-            print(f"Uh oh, an API error occurred. {e}")
-            return None
-        except Exception as e:
-            print(f"Uh oh, an error occurred: {e}")
-            return None
-
-    return summary
+    # Parallelize the chunk processing
+    with ThreadPoolExecutor(max_workers = 10) as executor:  # Adjust max_workers as needed
+        future_to_chunk = {executor.submit(ask_gpt_chunk, chunk): chunk for chunk in content_chunks}
+        return "".join(future.result() for future in as_completed(future_to_chunk))
