6edd8d8770573f82d545ddac57eb6d40e7f405e2
luckstrike <alexisgarc711@gmail.com>
2023-09-02 22:48:00 -0500

    Added basic styling to the Brainstorm mode

--- front-end/src/modes/Brainstorm.svelte
+++ front-end/src/modes/Brainstorm.svelte
@@ -14,7 +14,7 @@
 
 <div class="idea-generation-section">
     <!--Creating a textbox with placeholder text for brainstorming ideas-->
-    <input
+    <input id="idea-generation-input"
         type="text"
         placeholder="or try generating a random idea..."
         bind:value={brainstorm_text}
@@ -23,16 +23,55 @@
     <!--Creates a space between the input textbox and the buttons-->
     <br/>
     
-    <button on:click={() => handleGenerate()}>Generate</button>
-    <button 
-        on:click={() => handleGenerateRandomIdea()}
-        disabled={brainstorm_text !== ""}>
-        Generate Random Idea
-    </button>
+    <div class="button-container">
+        <button class="idea-buttons" on:click={() => handleGenerate()}>Generate Idea</button>
+        <button 
+            class="idea-buttons"
+            on:click={() => handleGenerateRandomIdea()}
+            disabled={brainstorm_text !== ""}>
+            Generate Random Idea
+        </button>
+    </div>
 </div>
 
 <style>
     button[disabled] {
         background-color: grey;
     }
+
+    button:not([disabled]):hover {
+        background-color: #005fa3;
+    }
+
+    .idea-generation-section {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        height: 100vh;
+    }
+
+    #idea-generation-input {
+        width: 50%;
+        height: 40px;
+        font-size: 18px;
+        box-sizing: border-box;
+        border-radius: 10px;
+        border: none;
+        padding: 0px 10px 0px 10px
+    }
+
+    .idea-buttons {
+        height: 40px;
+        padding: 10px;
+        font-size: 18px;
+        border-radius: 10px;
+        border: none;
+        margin: 0px 10px 0px 10px;
+        padding: 0px 10px 0px 10px;
+        background-color: #0076cb;
+        color: #ffffff;
+        font-weight: 550;
+    }
+
 </style>
