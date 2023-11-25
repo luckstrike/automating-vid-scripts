a4350c0c8a6dd4e910c4f9218ed0561af05c281d
ag0711 <ag0711@tamu.edu>
2023-11-24 23:55:18 -0600

    Added in more basic functionality to the text editor

--- front-end/src/modes/script/TextEditor.svelte
+++ front-end/src/modes/script/TextEditor.svelte
@@ -6,7 +6,11 @@
     let element: any; // figure out this type later
     let editor: any; // figure out this type later
 
+    // TODO: You might be able to take advantage of floating menus for GPT features!
+    // Use the floating menu to possibly suggest GPT features to the user on any new lines
+
     // NOTE: Extensions for TipTap can be added here
+
     onMount(() => {
         editor = new Editor({
         element: element,
@@ -28,22 +32,48 @@
     })
 </script>
 
+<!-- Let's get the following working: Bold, Italic, Underlined, Font Size -->
+<!-- Next Up: Bullet List -->
+
 {#if editor}
-  <button
-    on:click={() => editor.chain().focus().toggleHeading({ level: 1}).run()}
-    class:active={editor.isActive('heading', { level: 1 })}
-  >
-    H1
-  </button>
-  <button
-    on:click={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
-    class:active={editor.isActive('heading', { level: 2 })}
-  >
-    H2
-  </button>
-  <button on:click={() => editor.chain().focus().setParagraph().run()} class:active={editor.isActive('paragraph')}>
-    P
-  </button>
+    <!-- Bold Button -->
+    <button
+        on:click={() => console.log && editor.chain().focus().toggleBold().run()}
+        disabled={!editor.can().chain().focus().toggleBold().run()}
+        class={editor.isActive("bold") ? "is-active" : ""}
+    >
+        bold
+    </button>
+
+    <!-- Italic Button -->
+    <button
+        on:click={() => editor.chain().focus().toggleItalic().run()}
+        disabled={!editor.can().chain().focus().toggleItalic().run()}
+        class={editor.isActive("italic") ? "is-active" : ""}
+    >
+        italic
+    </button>
+
+    <!-- Paragraph Button -->
+    <button on:click={() => editor.chain().focus().setParagraph().run()} class:active={editor.isActive('paragraph')}>
+        P
+    </button>
+
+    <!-- Undo Button -->
+    <button
+        on:click={() => editor.chain().focus().undo().run()}
+        disabled={!editor.can().chain().focus().undo().run()}
+    >
+        undo
+    </button>
+
+    <!-- Redo Button -->
+    <button
+        on:click={() => editor.chain().focus().redo().run()}
+        disabled={!editor.can().chain().focus().redo().run()}
+    >
+        redo
+    </button>
 {/if}
 
 <div bind:this={element} />
@@ -52,6 +82,12 @@
   button.active {
     background: black;
     color: white;
+  }
+
+  .tiptap {
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    padding: 10px;
   }
 
 </style>
