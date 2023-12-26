b27af20f41350badc73c229e5e344d6d45c46684
ag0711 <ag0711@tamu.edu>
2023-12-25 22:42:09 -0600

    Added in basic sign up and login authentication with Firebase

--- svelte-kit/src/routes/+layout.svelte
+++ svelte-kit/src/routes/+layout.svelte
@@ -1,6 +1,24 @@
 <script lang="ts">
     import Sidebar from "$lib/Sidebar.svelte";
     import '../global.css';
+
+    import { onMount } from 'svelte';
+    import { auth } from '$lib/firebase/firebase.client';
+    import { authStore } from "$lib/stores/authStore";
+
+    onMount(() => {
+        const unsubscribe = auth.onAuthStateChanged((user) => {
+            console.log(user);
+            // Updates the current user in the authStore
+            authStore.update((curr) => {
+                return {
+                    ...curr,
+                    isLoading: false,
+                    currUser: user
+                }
+            });
+        });
+    })
 </script>
 
 <div class="container">
--- svelte-kit/src/routes/+page.svelte
+++ svelte-kit/src/routes/+page.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
 	import Sidebar from "$lib/Sidebar.svelte";
+	import Login from "$lib/Login.svelte";
 </script>
 
 <div class="container">
+	<Login />
 </div>
 
 <style>
+	.container {
+        display: flex;
+        height: 100vh;
+        flex: 0 0 100%;
+    }
 </style>
