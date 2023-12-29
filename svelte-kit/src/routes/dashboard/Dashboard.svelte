c1beaf05e749ce120df3f3cfcc467fec78662940
ag0711 <ag0711@tamu.edu>
2023-12-29 01:27:47 -0600

    Dashboard: Can now fetch data from Firestore

--- svelte-kit/src/routes/dashboard/Dashboard.svelte
+++ svelte-kit/src/routes/dashboard/Dashboard.svelte
@@ -1,8 +1,9 @@
 <script lang='ts'>
-    import { auth } from '$lib/firebase/firebase.client';
+    import { auth, db } from '$lib/firebase/firebase.client';
     
     import { authHandlers, authStore } from "$lib/stores/authStore";
 	import type { User } from 'firebase/auth';
+    import { collection, getDocs, query, where } from 'firebase/firestore';
     import { onMount } from 'svelte';
 
     let currentUser: User | null;
@@ -12,13 +13,45 @@
     // one of the script-boxes should be a button to make a new script
     // Then also list them in the script-list div
 
+    async function loadUserScripts() {
+        if (!currentUser) {
+            console.log("No user authenticated");
+            // Optionally, handle the case when there's no user (e.g., redirect to login)
+            return;
+        }
+
+        try {
+            const q = query(collection(db, "documents"), where("uid", "==", currentUser.uid));
+            const querySnapshot = await getDocs(q);
+
+
+            // This actually gets the documents
+            // The querySnapshot is an array of documents
+
+            querySnapshot.forEach((doc) => {
+                console.log(`${doc.id} => ${doc.data().doc_name}`);
+            });
+        } catch (error) {
+            console.error("Error loading scripts:", error);
+            // Handle the error appropriately
+        }
+    }
+
+
     onMount(() => {
         // This needs to be used to pull up the current user's scripts
         const unsubscribe = auth.onAuthStateChanged((user) => {
             currentUser = user;
-            console.log("Current User: ", currentUser);
-            // Updates the current user in the authStore
+            if (currentUser) {
+                // This is only called once the user has been resolved
+
+                // This is being called every time the dashboard is rendered though
+                // TODO: Figure out if there's a way to minimize this?
+                loadUserScripts();
+            }
         });
+
+        return unsubscribe;
     });
 
 </script>
