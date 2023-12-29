f78063c167e21b381ae7dcb9fc60e18101d89e1a
ag0711 <ag0711@tamu.edu>
2023-12-29 00:58:43 -0600

    Login page no longer appears briefly if you are logged in and at the root page

--- svelte-kit/src/routes/+page.svelte
+++ svelte-kit/src/routes/+page.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
 	import Sidebar from "$lib/Sidebar.svelte";
 	import Login from "$lib/Login.svelte";
+	import { authStore } from "$lib/stores/authStore";
 </script>
 
 <div class="container">
-	<Login />
+    {#if !$authStore.isLoading && !$authStore.currentUser}
+	    <Login />
+    {/if}
 </div>
 
 <style>
