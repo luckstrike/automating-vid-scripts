399e0209d9462426e618ede7e1ea43018d8e3bdf
ag0711 <ag0711@tamu.edu>
2023-12-30 14:48:29 -0600

    Dashboard: Added in test script boxes and styling

--- svelte-kit/src/routes/dashboard/Dashboard.svelte
+++ svelte-kit/src/routes/dashboard/Dashboard.svelte
@@ -6,12 +6,18 @@
     import { collection, getDocs, query, where } from 'firebase/firestore';
     import { onMount } from 'svelte';
 
+    // Icon Imports
+    import Fa from 'svelte-fa'
+    import { faPlus } from '@fortawesome/free-solid-svg-icons'
+
     let currentUser: User | null;
 
-    // TODO: Get the user's scripts from the database
-    // and display them in the script-boxes div (only show three max)
-    // one of the script-boxes should be a button to make a new script
-    // Then also list them in the script-list div
+    // TODO: Get the document titles and display them in the script-boxes div,
+    // organize them the most recent updated date
+    
+    // TODO: In Firestore, maybe move the content to a subcollection so it isn't all loaded at once?
+    // Right now Firestore is loading all of the document at once so in cases where the document
+    // is closer to 1MB, it's going to take a while to load
 
     async function loadUserScripts() {
         if (!currentUser) {
@@ -21,9 +27,9 @@
         }
 
         try {
+            // Only get the documents that belong to the current user
             const q = query(collection(db, "documents"), where("uid", "==", currentUser.uid));
             const querySnapshot = await getDocs(q);
-
 
             // This actually gets the documents
             // The querySnapshot is an array of documents
@@ -61,7 +67,26 @@
     <p class="title">or create a new script with Brainstorm or Summarize a URL!</p>
 
     <div class="script-boxes">
+        <div class="rectangle-container">
+            <div class="script-rectangle" id="new-script">
+                <Fa icon={faPlus} />
+            </div>
+            <div class="script-title">Create a New Script</div>
+        </div>
 
+        <!-- Use a loop here to create scripts, depending on how many scripts exist-->
+        <div class="rectangle-container">
+            <div class="script-rectangle"></div>
+            <div class="script-title">Title 1</div>
+        </div>
+        <div class="rectangle-container">
+            <div class="script-rectangle"></div>
+            <div class="script-title">Title 2</div>
+        </div>
+        <div class="rectangle-container">
+            <div class="script-rectangle"></div>
+            <div class="script-title">Title 3</div>
+        </div>
     </div>
 
     <div class="script-list">
@@ -84,6 +109,38 @@
         color: #fff;
         margin: 0;
         margin-top: 15px;
+    }
+    
+    .script-boxes {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: center;
+        align-items: center;
+        margin-top: 25px;
+    }
+
+    .script-rectangle {
+        width: 18.7vh; /* Trying to make this follow the 8.5 */
+        height: 24.2vh; /* Trying to make this follow the 11 */
+        background-color: #fff;
+        border-radius: 5px; /* Might remove the rounded corners */
+        margin-left: 25px;
+        margin-right: 25px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .script-title {
+        color: #fff;
+        margin-top: 10px;
+        text-align: center;
+    }
+
+    #new-script {
+        font-size: 36px;
+        color: #2f2f2f;
     }
 
 </style>
