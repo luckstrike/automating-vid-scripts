230b4c3eea4c0771d9e29e428d82ece7b89fa94e
ag0711 <ag0711@tamu.edu>
2024-08-23 13:56:34 -0500

    Added in Tailwind CSS

--- svelte-kit/src/lib/Login.svelte
+++ svelte-kit/src/lib/Login.svelte
@@ -1,164 +1,179 @@
-<script lang='ts'>
-	import { authHandlers, authStore } from "./stores/authStore";
-    import DeviconGoogle from '~icons/devicon/google';
+<script lang="ts">
+	import { authHandlers, authStore } from './stores/authStore';
+	import DeviconGoogle from '~icons/devicon/google';
 
-    // TODO: Add in Google Auth log in
-    // You can probably have a button that says "Log in with Google"
-    // which then calls the authHandlers.loginWithGoogle() function
+	// TODO: Add in Google Auth log in
+	// You can probably have a button that says "Log in with Google"
+	// which then calls the authHandlers.loginWithGoogle() function
 
-    let signUp: boolean = false; // log in is the default mode
+	let signUp: boolean = false; // log in is the default mode
 
-    let email: string = '';
-    let password: string = '';
-    let confirmPassword: string = '';
+	let email: string = '';
+	let password: string = '';
+	let confirmPassword: string = '';
 
-    async function handleSubmit() {
-        if (!email || !password) {
-            alert('Please enter an email and password');
-            return;
-        } else if (signUp && !confirmPassword) {
-            alert('Please confirm your password');
-            return;
-        } else if (signUp && password !== confirmPassword) {
-            alert('Passwords do not match, please make sure they are the same');
-            return;
-        }
+	async function handleSubmit() {
+		if (!email || !password) {
+			alert('Please enter an email and password');
+			return;
+		} else if (signUp && !confirmPassword) {
+			alert('Please confirm your password');
+			return;
+		} else if (signUp && password !== confirmPassword) {
+			alert('Passwords do not match, please make sure they are the same');
+			return;
+		}
 
-        if (signUp && password === confirmPassword) {
-            // Sign Up logic
-            try {
-                await authHandlers.signup(email, password);
-            } catch (error) {
-                console.log(error);
-            }
-        } else {
-            // Log In logic
-            try {
-                await authHandlers.login(email, password);
-            } catch (error) {
-                console.log(error);
-            }
-        }
+		if (signUp && password === confirmPassword) {
+			// Sign Up logic
+			try {
+				await authHandlers.signup(email, password);
+			} catch (error) {
+				console.log(error);
+			}
+		} else {
+			// Log In logic
+			try {
+				await authHandlers.login(email, password);
+			} catch (error) {
+				console.log(error);
+			}
+		}
 
-        if ($authStore.currentUser) {
-            window.location.href = '/dashboard';
-        }
-        console.log("???:", $authStore.currentUser);
-    }
+		if ($authStore.currentUser) {
+			window.location.href = '/dashboard';
+		}
+		console.log('???:', $authStore.currentUser);
+	}
 
-    async function handleGoogleSignIn() {
-        try {
-            // Try logging in the user with Google
-            await authHandlers.loginWithGoogle();
+	async function handleGoogleSignIn() {
+		try {
+			// Try logging in the user with Google
+			await authHandlers.loginWithGoogle();
 
-            // If the user is authenticated then redirect them to
-            // the dashboard
-            if ($authStore.currentUser) {
-                window.location.href = '/dashboard';
-            }
-        } catch (error) {
-            console.error("Google sign-in failed", error);
-        }
-    }
+			// If the user is authenticated then redirect them to
+			// the dashboard
+			if ($authStore.currentUser) {
+				window.location.href = '/dashboard';
+			}
+		} catch (error) {
+			console.error('Google sign-in failed', error);
+		}
+	}
 </script>
 
 <div class="container">
-    <h1 class="welcome-title">Howdy there, welcome to VidScripts</h1>
-    <h2 class="welcome-title">{signUp ? 'Sign Up' : 'Log In'}</h2>
-    <form>
-        <input bind:value={email} class="info-input" type="text" placeholder="Email" />
-        <input bind:value={password} class="info-input" type="password" placeholder="Password" />
+	<h1 class="welcome-title">Howdy there, welcome to VidScripts</h1>
+	<h2 class="welcome-title">{signUp ? 'Sign Up' : 'Log In'}</h2>
+	<button class="google-login" on:click={handleGoogleSignIn}>
+		<DeviconGoogle style="font-size: 1.5em"></DeviconGoogle>
+		Continue with Google
+	</button>
+	<form>
+		<input bind:value={email} class="info-input" type="text" placeholder="Email" />
+		<input bind:value={password} class="info-input" type="password" placeholder="Password" />
 
-        {#if signUp}
-            <input bind:value={confirmPassword} class="info-input" type="password" placeholder="Confirm Password" />
-        {/if}
+		{#if signUp}
+			<input
+				bind:value={confirmPassword}
+				class="info-input"
+				type="password"
+				placeholder="Confirm Password"
+			/>
+		{/if}
 
-        <button on:click|preventDefault={handleSubmit} class="action" type="submit">
-            {#if signUp}
-                Sign Up
-            {:else}
-                Log In
-            {/if}
-        </button>
+		<button on:click|preventDefault={handleSubmit} class="action" type="submit">
+			{#if signUp}
+				Sign Up
+			{:else}
+				Log In
+			{/if}
+		</button>
 
-        {#if signUp}
-            Already have an account?
-            <button class="switch-mode" on:click={() => {signUp = false;}}>
-                <p class="switch-text">Log In</p>
-            </button>
-        {:else}
-            Or Login With...
-            <button class="google-login" on:click={handleGoogleSignIn}>              
-              <DeviconGoogle style="font-size: 1.5em"></DeviconGoogle>
-            </button>
-            <div>
-              Don't have an account?
-            </div>
-            <button class="switch-mode" on:click={() => {signUp = true;}}>
-                <p class="switch-text">Sign Up</p>
-            </button>
-        {/if}
-
-    </form>
+		{#if signUp}
+			Already have an account?
+			<button
+				class="switch-mode"
+				on:click={() => {
+					signUp = false;
+				}}
+			>
+				<p class="switch-text">Log In</p>
+			</button>
+		{:else}
+			Or Login With...
+			<div>Don't have an account?</div>
+			<button
+				class="switch-mode"
+				on:click={() => {
+					signUp = true;
+				}}
+			>
+				<p class="switch-text">Sign Up</p>
+			</button>
+		{/if}
+	</form>
 </div>
 
 <style>
-    .welcome-title {
-        color: #fff;
-    }
+	.welcome-title {
+		color: #fff;
+	}
 
-    .container {
-        display: flex;
-        flex-direction: column;
-        flex: 0 0 100%;
-        height: 100vh;
-        justify-content: center;
-        align-items: center;
-    }
+	.container {
+		display: flex;
+		flex-direction: column;
+		flex: 0 0 100%;
+		height: 100vh;
+		justify-content: center;
+		align-items: center;
+	}
 
-    .container form {
-        display: flex;
-        flex-direction: column;
-        flex: 1;
-        text-align: center;
-    }
+	.container form {
+		display: flex;
+		flex-direction: column;
+		flex: 1;
+		text-align: center;
+	}
 
-    .switch-mode {
-        border: none;
-        cursor: pointer;
-        color: #0070f3;
-        background-color: transparent;
-        padding: 0;
-        margin: 0;
-    }
+	.switch-mode {
+		border: none;
+		cursor: pointer;
+		color: #0070f3;
+		background-color: transparent;
+		padding: 0;
+		margin: 0;
+	}
 
-    .info-input {
-        border: none;
-        border-radius: 10px;
-        width: 300px;
-        height: 40px;
-        margin: 5px;
-    }
+	.info-input {
+		border: none;
+		border-radius: 10px;
+		width: 300px;
+		height: 40px;
+		margin: 5px;
+	}
 
-    .action {
-        border: none;
-        border-radius: 10px;
-        width: 300px;
-        height: 40px;
-        margin: 5px;
-        background-color: #0070f3;
-    }
+	.action {
+		border: none;
+		border-radius: 10px;
+		width: 300px;
+		height: 40px;
+		margin: 5px;
+		background-color: #0070f3;
+	}
 
-    .switch-text {
-        text-decoration: none;
-        color: #0070f3;
-    }
+	.switch-text {
+		text-decoration: none;
+		color: #0070f3;
+	}
 
-    .google-login {
-        background-color: #0070f3;
-        border-radius: 10px;
-        border: none;
-        padding: 5px;
-        flex-direction: row;
-    }
+	.google-login {
+		background-color: #0070f3;
+		border-radius: 10px;
+		border: none;
+		padding: 5px;
+		flex-direction: column;
+		width: 25%;
+		text-align: center;
+	}
 </style>
--- svelte-kit/src/routes/+layout.svelte
+++ svelte-kit/src/routes/+layout.svelte
@@ -1,70 +1,81 @@
 <script lang="ts">
-    import Sidebar from "$lib/Sidebar.svelte";
-    import '../global.css';
+	import Sidebar from '$lib/Sidebar.svelte';
+	import '../global.css';
+	import '../app.css';
 
-    import { onMount } from 'svelte';
-    import { auth } from '$lib/firebase/firebase.client';
-    import { authStore } from "$lib/stores/authStore";
-    import { scriptSaveStatus } from "$lib/stores/scriptStore"
-    import { browser } from '$app/environment';
-    import { page } from '$app/stores'
+	import { onMount } from 'svelte';
+	import { auth } from '$lib/firebase/firebase.client';
+	import { authStore } from '$lib/stores/authStore';
+	import { scriptSaveStatus } from '$lib/stores/scriptStore';
+	import { browser } from '$app/environment';
+	import { page } from '$app/stores';
 
-    // This sets the scriptSaveStatus store to false whenever it
-    // isn't the route being accessed (basically resetting it)
-    // TODO: If I ever want to keep the text editors states
-    //       while changing states I will need to remove this
-    $: {
-            if (!$page.url.pathname.startsWith('/script')) {
-                scriptSaveStatus.set(false);
-                console.log("routes/+layout.svelte: Resetting the scriptSaveStatus to false!")
-            }
-        }
+	// This sets the scriptSaveStatus store to false whenever it
+	// isn't the route being accessed (basically resetting it)
+	// TODO: If I ever want to keep the text editors states
+	//       while changing states I will need to remove this
+	$: {
+		if (!$page.url.pathname.startsWith('/script')) {
+			scriptSaveStatus.set(false);
+			console.log('routes/+layout.svelte: Resetting the scriptSaveStatus to false!');
+		}
+	}
 
-    onMount(() => {
-        const unsubscribe = auth.onAuthStateChanged((user) => {
-            console.log("Current User: ", user);
-            // Updates the current user in the authStore
-            // TODO: Change any to User (I need to find out how to use the User type from firebase)
+	onMount(() => {
+		const unsubscribe = auth.onAuthStateChanged((user) => {
+			console.log('Current User: ', user);
+			// Updates the current user in the authStore
+			// TODO: Change any to User (I need to find out how to use the User type from firebase)
 
-            authStore.update((curr: any) => {
-                return {
-                    ...curr,
-                    isLoading: false,
-                    currentUser: user
-                }
-            });
+			authStore.update((curr: any) => {
+				return {
+					...curr,
+					isLoading: false,
+					currentUser: user
+				};
+			});
 
-            // Takes the user back to the root (login) page if they are not logged in
-            if (browser && !$authStore.currentUser && !$authStore.isLoading && window.location.pathname != '/') {
-                window.location.href = '/';
-            } else if (browser && $authStore.currentUser && !$authStore.isLoading && window.location.pathname == '/') {
-                window.location.href = '/dashboard';
-            }
-        });
+			// Takes the user back to the root (login) page if they are not logged in
+			if (
+				browser &&
+				!$authStore.currentUser &&
+				!$authStore.isLoading &&
+				window.location.pathname != '/'
+			) {
+				window.location.href = '/';
+			} else if (
+				browser &&
+				$authStore.currentUser &&
+				!$authStore.isLoading &&
+				window.location.pathname == '/'
+			) {
+				window.location.href = '/dashboard';
+			}
+		});
 
-        return unsubscribe;
-    })
+		return unsubscribe;
+	});
 </script>
 
 <div class="layout-container">
-    {#if $authStore.currentUser}
-        <Sidebar />
-    {/if}
+	{#if $authStore.currentUser}
+		<Sidebar />
+	{/if}
 
-    <main class="main-content">
-        <slot />
-    </main>
+	<main class="main-content">
+		<slot />
+	</main>
 </div>
 
 <style>
-    .layout-container {
-        display: flex;
-        height: 100vh;
-    }
+	.layout-container {
+		display: flex;
+		height: 100vh;
+	}
 
-    .main-content {
-        flex: 1; /* Take up remaining space */
-        overflow: auto; /* Scroll if content overflows */
-    }
-
+	.main-content {
+		flex: 1; /* Take up remaining space */
+		overflow: auto; /* Scroll if content overflows */
+	}
 </style>
+
