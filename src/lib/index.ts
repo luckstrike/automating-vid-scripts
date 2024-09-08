93eb7a53d353c039df81115e36045e0f4451b3d0
ag0711 <ag0711@tamu.edu>
2024-09-07 23:18:17 -0500

    Started to add in the back-end API part for the bubble & floating menu

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,67 +3,71 @@ import type { DocumentData, DocumentReference, FirestoreDataConverter, QueryDocu
 
 // Interfaces
 export interface Script {
-    name: string;
-    lastUpdatedString: string;
-    lastUpdatedDate: Date | null;
-    content: string | null | unknown; // a reference to the actual content (so less data is used each call)
-    metaDocId: string | null | unknown; // id of this entry (contains a script's metadata)
+  name: string;
+  lastUpdatedString: string;
+  lastUpdatedDate: Date | null;
+  content: string | null | unknown; // a reference to the actual content (so less data is used each call)
+  metaDocId: string | null | unknown; // id of this entry (contains a script's metadata)
 }
 
 export interface TextContent {
-    content: string,
-    uid: string
+  content: string,
+  uid: string
 }
 
 // POTENTIAL TODO: Combine Script and ScriptMetaData
 export interface ScriptMetaData {
-    content: DocumentReference<TextContent>,
-    created: Timestamp,
-    doc_name: string,
-    uid: string,
-    updated: Timestamp
+  content: DocumentReference<TextContent>,
+  created: Timestamp,
+  doc_name: string,
+  uid: string,
+  updated: Timestamp
 }
 
 export interface UserPrompt {
-    prompt?: string
+  prompt?: string
 }
 
 export interface UserProvidedURL {
-    url?: string
+  url?: string
+}
+
+export interface UserSelection {
+  userSelection?: string
 }
 
 // Converters
 export const textContentConverter: FirestoreDataConverter<TextContent> = {
-    toFirestore(textContent: TextContent): DocumentData {
-        return { ...textContent };
-    },
-    fromFirestore(
-        snapshot: QueryDocumentSnapshot,
-        options: SnapshotOptions
-    ): TextContent {
-        const data = snapshot.data(options)!;
-        return {
-            content: data.content,
-            uid: data.uid
-        };
-    }
+  toFirestore(textContent: TextContent): DocumentData {
+    return { ...textContent };
+  },
+  fromFirestore(
+    snapshot: QueryDocumentSnapshot,
+    options: SnapshotOptions
+  ): TextContent {
+    const data = snapshot.data(options)!;
+    return {
+      content: data.content,
+      uid: data.uid
+    };
+  }
 };
 
 export const scriptMetaDataConverter: FirestoreDataConverter<ScriptMetaData> = {
-    toFirestore(scriptMetaData: ScriptMetaData): DocumentData {
-        return { ...scriptMetaData };
-    },
-    fromFirestore(
-        snapshot: QueryDocumentSnapshot,
-        options: SnapshotOptions
-    ): ScriptMetaData {
-        const data = snapshot.data(options)!;
-        return {
-            content: data.content,
-            created: data.created,
-            doc_name: data.doc_name,
-            uid: data.uid,
-            updated: data.updated
-        };
-    }
+  toFirestore(scriptMetaData: ScriptMetaData): DocumentData {
+    return { ...scriptMetaData };
+  },
+  fromFirestore(
+    snapshot: QueryDocumentSnapshot,
+    options: SnapshotOptions
+  ): ScriptMetaData {
+    const data = snapshot.data(options)!;
+    return {
+      content: data.content,
+      created: data.created,
+      doc_name: data.doc_name,
+      uid: data.uid,
+      updated: data.updated
+    };
+  }
 };
---
+++ src/routes/api/script/+server.ts
@@ -0,0 +1,58 @@
+import type { UserSelection } from '$lib';
+import { json, type RequestHandler } from '@sveltejs/kit'
+import OpenAI from "openai";
+import * as dotenv from 'dotenv';
+
+dotenv.config()
+
+// Setting up access to the OpenAI API
+const OPENAI_API_KEY: string | undefined = process.env.OPENAI_API_KEY;
+const openai = new OpenAI({ apiKey: OPENAI_API_KEY });
+
+const generatePrompt: string = `You are an excellent writer who can reword and expand on any type of text. A user
+                                has provided you with text that you are to help them rewrite. If the text is too short
+                                to expand upon or rewrite you can simply just return the text back and write something
+                                in your response like [not enough text provided]. The content you receive may or may not
+                                be formatted in HTML so please return any results in HTML formatting.`
+
+async function sendToGPT(openai: OpenAI, prompt: string, userInput: string): Promise<string | null> {
+  const completion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: "system",
+        content: prompt
+      },
+      {
+        role: "user",
+        content: userInput
+      }
+    ],
+    model: "gpt-4o-mini",
+  });
+
+  return completion.choices[0].message.content;
+}
+
+export const POST: RequestHandler = async ({ request }) => {
+
+  let data: UserSelection | null = null;
+
+  try {
+    data = await request.json() as UserSelection;
+  } catch (e) {
+    console.error("Unable to handle a POST request /api/script")
+    return json({ success: false, error: "Bad request" }, { status: 400 })
+  }
+
+  // Use the OpenAI API here
+  let gptResult: string | null = "";
+
+  if (data && data.userSelection) {
+    gptResult = await sendToGPT(openai, generatePrompt, data.userSelection);
+  }
+  if (gptResult) {
+    return json({ success: true, gptContent: gptResult })
+  } else {
+    return json({ success: false, gptContent: "" })
+  }
+}
