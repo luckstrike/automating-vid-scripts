6ec773bccf63bbef2d5019b09d5332ca04881404
ag0711 <ag0711@tamu.edu>
2024-09-16 22:18:27 -0500

    Threw each slide into a div (if I didn't that would probably burn me later)

--- src/lib/Landing.svelte
+++ src/lib/Landing.svelte
@@ -25,40 +25,48 @@
 
     {#if slide == 0}
       <div
-        class="max-w-[30%] text-7xl font-bold bg-gradient-to-b from-[#5CA4ED] to-[#97D9E1] text-transparent bg-clip-text"
+        class="flex flex-row w-screen h-screen justify-center items-center space-x-8"
       >
-        Write your video scripts with a helping hand
-      </div>
-      <div
-        class="w-[43%] h-[100%] flex flex-col bg-[#d9d9d9] rounded-lg translate-y-[15%] p-8"
-      >
-        <div class="font-bold text-3xl">The Generational Ford Mustang</div>
-        <br />
-        <br />
-        <div class="text-3xl select-all">
-          The Ford Mustang has been around the block for a long time. In fact
-          it’s gone through a lot of generations throughout it’s existence.
+        <div
+          class="max-w-[30%] text-7xl font-bold bg-gradient-to-b from-[#5CA4ED] to-[#97D9E1] text-transparent bg-clip-text"
+        >
+          Write your video scripts with a helping hand
+        </div>
+        <div
+          class="w-[45%] h-[100%] flex flex-col bg-[#d9d9d9] rounded-lg translate-y-[15%] p-8"
+        >
+          <div class="font-bold text-3xl">The Generational Ford Mustang</div>
+          <br />
+          <br />
+          <div class="text-3xl select-all">
+            The Ford Mustang has been around the block for a long time. In fact
+            it’s gone through a lot of generations throughout it’s existence.
+          </div>
         </div>
       </div>
     {/if}
 
     {#if slide == 1}
-      <div
-        class="max-w-[30%] text-7xl font-bold bg-gradient-to-b from-[#5CA4ED] to-[#97D9E1] text-transparent bg-clip-text"
-      >
-        Writer's block? No Problem
+      <div class="flex flex-row w-screen h-screen justify-center items-center">
+        <div
+          class="max-w-[30%] text-7xl font-bold bg-gradient-to-b from-[#5CA4ED] to-[#97D9E1] text-transparent bg-clip-text"
+        >
+          Writer's block? No Problem
+        </div>
       </div>
     {/if}
 
     {#if slide == 2}
-      <div class="max-w-[30%]">
-        <div
-          class="text-7xl font-bold bg-gradient-to-b from-[#5CA4ED] to-[#97D9E1] text-transparent bg-clip-text"
-        >
-          Long article you wanna use? Easy*
-        </div>
-        <div class="text-white">
-          *In cases where the site allows for web scraping
+      <div class="flex flex-row w-screen h-screen justify-center items-center">
+        <div class="max-w-[30%]">
+          <div
+            class="text-7xl font-bold bg-gradient-to-b from-[#5CA4ED] to-[#97D9E1] text-transparent bg-clip-text"
+          >
+            Long article you wanna use? Easy*
+          </div>
+          <div class="text-white">
+            *In cases where the site allows for web scraping
+          </div>
         </div>
       </div>
     {/if}
