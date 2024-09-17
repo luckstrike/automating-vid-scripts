2210d34e5f42ece8eb6ab896a938bb7a835c4627
ag0711 <ag0711@tamu.edu>
2024-09-16 21:15:46 -0500

    Started to replace the login page being first shown with a landing page

---
+++ src/lib/Landing.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import EmojioneMonotoneLeftArrow from "~icons/emojione-monotone/left-arrow";
+  import EmojioneMonotoneRightArrow from "~icons/emojione-monotone/right-arrow";
+</script>
+
+<div class="flex flex-col w-screen h-screen p-2">
+  <div class="flex flex-row space-x-4 justify-end items-center p-2 text-white">
+    <div>Home</div>
+    <button class="bg-blue-600 rounded-lg pt-2 pb-2 pl-4 pr-4 font-semibold"
+      >Login</button
+    >
+  </div>
+  <div class="flex flex-row flex-grow items-center justify-between">
+    <button>
+      <EmojioneMonotoneLeftArrow class="text-4xl text-blue-600" />
+    </button>
+    <div
+      class="max-w-[30%] text-7xl font-bold bg-gradient-to-b from-[#5CA4ED] to-[#97D9E1] text-transparent bg-clip-text"
+    >
+      Write your video scripts with a helping hand
+    </div>
+    <div
+      class="max-w-[37%] h-[90%] flex flex-grow bg-[#d9d9d9] rounded-lg"
+    ></div>
+    <button>
+      <EmojioneMonotoneRightArrow class="text-4xl text-blue-600" />
+    </button>
+  </div>
+</div>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import Login from "$lib/Login.svelte";
+  import { authStore } from "$lib/stores/authStore";
+</script>
+
+<div class="flex h-screen w-screen">
+  {#if !$authStore.isLoading && !$authStore.currentUser}
+    <Login />
+  {/if}
+</div>
