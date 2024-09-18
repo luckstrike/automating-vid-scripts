5aecae9cb106e2b4995803a82c81dd344a3a3e50
luckstrike <alexisgarc711@gmail.com>
2024-09-17 20:14:35 -0500

    Added to content to the rest of the landing pages + fixed an issue with the login page

--- src/lib/Landing.svelte
+++ src/lib/Landing.svelte
@@ -2,6 +2,7 @@
   import EmojioneMonotoneLeftArrow from "~icons/emojione-monotone/left-arrow";
   import EmojioneMonotoneRightArrow from "~icons/emojione-monotone/right-arrow";
   import { goto } from "$app/navigation";
+  import { authStore } from "$lib/stores/authStore";
 
   // Used to determine which "slide" to show the user
   let slide: number = 0;
@@ -10,7 +11,9 @@
 
 <div class="flex flex-col w-screen h-screen p-2">
   <div class="flex flex-row space-x-4 justify-end items-center p-2 text-white">
-    <div>Home</div>
+    <button on:click={() => goto("/")}>
+      Home
+    </button>
     <button
       class="bg-blue-600 rounded-lg pt-2 pb-2 pl-4 pr-4 font-semibold"
       on:click={() => goto("/login")}
@@ -21,7 +24,7 @@
   <div
     class="flex flex-row flex-grow items-center justify-between pl-4 pr-4 overflow-y-hidden"
   >
-    <button on:click={() => (slide = (slide - 1) % maxSlides)}>
+    <button on:click={() => slide = (slide - 1) % maxSlides}>
       <EmojioneMonotoneLeftArrow class="text-4xl text-blue-600" />
     </button>
 
@@ -48,17 +51,42 @@
       </div>
     {/if}
 
-    {#if slide == 1}
-      <div class="flex flex-row w-screen h-full justify-center items-center">
+    {#if Math.abs(slide) == 1}
+      <div class="flex flex-row w-screen h-full justify-center items-center space-x-4">
         <div
           class="max-w-[30%] text-7xl font-bold bg-gradient-to-b from-[#5CA4ED] to-[#97D9E1] text-transparent bg-clip-text"
         >
           Writer's block? No Problem
         </div>
+        <div class="flex flex-col max-w-[45%] items-center align-middle justify-center text-white p-4">
+          <!--Creating a textbox with placeholder text for brainstorming ideas-->
+          <div class="text-4xl font-bold py-4">Kickstart your writing process!</div>
+          <div class="text-lg">
+            Do you have a topic that you want to make a video about? Provide it here:
+          </div>
+          <input
+            class="w-full h-10 rounded-lg px-2 text-black"
+            type="text"
+            placeholder="or try generating a random idea..."
+          />
+
+          <div class="flex flex-row p-2 space-x-2">
+            <button
+              class="h-10 px-2 rounded-lg bg-blue-600"
+            >
+              Generate Idea
+            </button>
+            <button
+              class="h-10 px-2 rounded-lg bg-blue-600"
+            >
+              Generate Random Idea
+            </button>
+          </div>
+        </div>
       </div>
     {/if}
 
-    {#if slide == 2}
+    {#if Math.abs(slide) == 2}
       <div class="flex flex-row w-screen h-full justify-center items-center">
         <div class="max-w-[30%]">
           <div
@@ -70,10 +98,34 @@
             *In cases where the site allows for web scraping
           </div>
         </div>
+        <div
+          class="flex flex-col max-w-[45%] text-white space-y-3 items-center align-middle justify-center"
+        >
+          <div class="text-4xl font-bold">
+            Ready to summarize a page?
+          </div>
+          <p class="text-lg">
+            Please provide the URL of the website you would like to summarize:
+          </p>
+          <input
+            class="text-black w-full rounded-xl p-2 box-border border-none font-xl"
+            type="text"
+            placeholder="Enter URL here..."
+          />
+          <!-- Why the -mt-3? Since I'm using space-y-3 this causes that to also be applied to the 
+              overlay, so adding this in fixes the issue some-->
+
+          <button
+            class="p-2 disabled:bg-gray-400 disabled:cursor-not-allowed bg-blue-600 rounded-lg text-white"
+          >
+            Summarize
+          </button>
+        </div>
+
       </div>
     {/if}
 
-    <button on:click={() => (slide = (slide + 1) % maxSlides)}>
+    <button on:click={() => slide = (slide + 1) % maxSlides}>
       <EmojioneMonotoneRightArrow class="text-4xl text-blue-600" />
     </button>
   </div>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,6 +1,18 @@
 <script lang="ts">
   import Login from "$lib/Login.svelte";
   import { authStore } from "$lib/stores/authStore";
+  import { goto } from "$app/navigation";
+  import { onMount } from 'svelte';
+
+  function handleAuthState(user) {
+    if (user) {
+      goto("/dashboard")
+    }
+  }
+
+  $: if (!authStore.isLoading) {
+    handleAuthState($authStore.currentUser);
+  }
 </script>
 
 <div class="flex h-screen w-screen">
