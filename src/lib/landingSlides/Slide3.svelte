b8f471fbfc1e15f2ea5d52b0bf8db8df86c6a2a3
luckstrike <alexisgarc711@gmail.com>
2024-09-19 22:41:05 -0500

    Slight improvements to how the third slide looks on smaller screens

--- src/lib/landingSlides/Slide3.svelte
+++ src/lib/landingSlides/Slide3.svelte
@@ -4,7 +4,7 @@
 <div
   class="flex flex-col flex-grow overflow-y-auto min-h-full lg:flex-row lg:w-full justify-center space-y-8 lg:space-y-0 items-center p-4"
 >
-  <div class="lg:max-w-[30%]">
+  <div class="lg:max-w-[30%] space-y-2">
     <div
       class="text-center text-5xl font-bold bg-gradient-to-b from-[#5CA4ED] to-[#97D9E1] text-transparent bg-clip-text"
     >
@@ -15,10 +15,9 @@
     </div>
   </div>
   <div
-    class="flex flex-col lg:max-w-[45%] text-white items-center align-middle justify-center"
+    class="flex flex-col lg:max-w-[45%] text-white items-center space-y-2 align-middle justify-center"
   >
-    <div class="text-center text-4xl font-bold">Ready to summarize a page?</div>
-    <p class="text-lg">
+    <p class="text-center text-lg">
       Please provide the URL of the website you would like to summarize:
     </p>
     <input
