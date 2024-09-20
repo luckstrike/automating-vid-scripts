31f3c1ec5fbe9f18113a43a191f7d564fc390487
ag0711 <ag0711@tamu.edu>
2024-09-20 18:22:17 -0500

    Started working on making scripting workspace responsive on smaller screens

---
+++ src/lib/HamburgerMenu.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  import { goto } from "$app/navigation";
+  import { fade } from "svelte/transition";
+  let isOpen = false;
+
+  const toggleMenu = () => {
+    isOpen = !isOpen;
+  };
+
+  function changeTabs(subdirectory: string) {
+    goto(subdirectory);
+    isOpen = !isOpen;
+  }
+</script>
+
+<div class="flex flex-col">
+  <div class="text-right">
+    <button
+      on:click={toggleMenu}
+      class="text-4xl text-white p-2 focus:outline-none"
+    >
+      ☰
+    </button>
+  </div>
+  {#if isOpen}
+    <div class="">
+      <div
+        transition:fade={{ duration: 100 }}
+        class="fixed inset-0 bg-gray-800 bg-opacity-90 z-50"
+      >
+        <div
+          class="flex flex-col items-center justify-center h-full text-white"
+        >
+          <button on:click={toggleMenu} class="absolute top-4 right-4 text-2xl">
+            X
+          </button>
+          <button on:click={() => changeTabs("/dashboard")}>Dashboard</button>
+          <button on:click={() => changeTabs("/brainstorm")}>Brainstorm</button>
+          <button on:click={() => changeTabs("/script")}>Script</button>
+          <button on:click={() => changeTabs("/summarize")}>Summarize</button>
+          <button on:click={() => changeTabs("/")}>Log Out</button>
+        </div>
+      </div>
+    </div>
+  {/if}
+</div>
--- src/lib/Sidebar.svelte
+++ src/lib/Sidebar.svelte
@@ -1,96 +1,21 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import { goto } from '$app/navigation';
-	import { page } from '$app/stores';
-	import { authHandlers } from './stores/authStore';
-	import { scriptMetaIdStore, scriptSaveStatus } from './stores/scriptStore';
-	import { get } from 'svelte/store';
-
-	function activateLink(index: number, event: MouseEvent) {
-		const status = get(scriptSaveStatus);
-
-		if (
-			!status ||
-			confirm(
-				'You have unsaved script changes that will be lost upon leaving this page. Are you sure you want to exit this tab?'
-			)
-		) {
-			const path = links[index].anchor;
-
-			goto(path);
-		} else {
-			// Prevent's the default event of going to a link from happening
-			event.preventDefault();
-
-			// Stops the event from propagating further
-			event.stopPropagation();
-		}
-
-		links = links.map((link, i) => ({
-			...link,
-			isActive: i === index
-		}));
-	}
-
-	// Creating the elements for the sidebar
-	let links = [
-		{ name: 'Dashboard', color: '#FFD700', anchor: '/dashboard', isActive: false },
-		{ name: 'Brainstorm', color: '#990000', anchor: '/brainstorm', isActive: false },
-		{ name: 'Script', color: '#107500', anchor: '/script', isActive: false },
-		{ name: 'Summarize', color: '#003d75', anchor: '/summarize', isActive: false },
-		{ name: 'Log Out', color: '#ffffff', anchor: '/', isActive: false }
-	];
-
-	// Activating the link that was clicked
-	$: {
-		const currentAnchor = $page.url.pathname;
-		links = links.map((link) => ({
-			...link,
-			isActive: currentAnchor === link.anchor
-		}));
-	}
+  import { goto } from "$app/navigation";
 </script>
 
-<div class="h-screen bg-gray-900 overflow-x-hidden flex-none w-1/6 font-sans">
-	<!-- Creating the sidebar elements with Svelte -->
-	<!-- Use link.anchors here -->
-	{#each links as link, index}
-		{#if link.name === 'Log Out'}
-			<!-- If the link is the logging out block -->
-			<a
-				href={link.anchor}
-				data-index={index}
-				class="block py-5 px-2 text-xl text-gray-100 text-center transition-all duration-300 ease-in-out hover:font-bold hover:bg-gray-800 relative group {link.isActive
-					? 'bg-gray-800'
-					: ''}"
-				on:click={() => authHandlers.logout()}
-			>
-				{link.name}
-				<div
-					class="absolute top-0 right-0 w-2.5 h-full {link.color} opacity-0 group-hover:opacity-100 {link.isActive
-						? 'opacity-100'
-						: ''} transition-opacity duration-300"
-				></div>
-			</a>
-		{:else if link.name == 'Script' && (!$scriptMetaIdStore || !$scriptMetaIdStore)}
-			<!-- Don't render anything, as there is no active script here to load -->
-		{:else}
-			<a
-				href={link.anchor}
-				data-index={index}
-				class="block py-5 px-2 text-xl text-gray-100 text-center transition-all duration-300 ease-in-out hover:font-bold hover:bg-gray-800 relative group {link.isActive
-					? 'bg-gray-800'
-					: ''}"
-				on:click|preventDefault={(event) => activateLink(index, event)}
-			>
-				{link.name}
-				<div
-					class="absolute top-0 right-0 w-2.5 h-full {link.color} opacity-0 group-hover:opacity-100 {link.isActive
-						? 'opacity-100'
-						: ''} transition-opacity duration-300"
-				></div>
-			</a>
-		{/if}
-	{/each}
+<div class="flex flex-col h-full w-64 text-xl text-white bg-[#1f1f1f]">
+  <button class="h-16 hover:bg-gray-600" on:click={() => goto("/dashboard")}
+    >Dashboard</button
+  >
+  <button class="h-16 hover:bg-gray-600" on:click={() => goto("/brainstorm")}
+    >Brainstorm</button
+  >
+  <button class="h-16 hover:bg-gray-600" on:click={() => goto("/script")}
+    >Script</button
+  >
+  <button class="h-16 hover:bg-gray-600" on:click={() => goto("/summarize")}
+    >Summarize</button
+  >
+  <button class="h-16 hover:bg-gray-600" on:click={() => goto("/")}
+    >Log Out</button
+  >
 </div>
-
