e40c132ce8498fda9ecaa5bd289629e2c05fff7b
ag0711 <ag0711@tamu.edu>
2024-09-20 16:23:40 -0500

    Fixed the login page having an off centered Continue with Google button

--- src/lib/Login.svelte
+++ src/lib/Login.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { authHandlers, authStore } from "./stores/authStore";
+  import { goto } from "$app/navigation";
   import DeviconGoogle from "~icons/devicon/google";
 
   // TODO: Add in Google Auth log in
@@ -62,79 +63,93 @@
   }
 </script>
 
-<div
-  class="flex flex-col h-screen w-screen justify-center items-center space-y-2 p-4"
->
-  <div class="text-center text-4xl font-bold text-white">
-    Howdy there, welcome to VidScripts
+<div class="flex flex-col h-full w-full p-2">
+  <div class="flex flex-row space-x-4 justify-end text-white mb-4">
+    <button on:click={() => goto("/")}> Home </button>
+    <button
+      class="bg-blue-600 rounded-lg pt-2 pb-2 pl-4 pr-4 font-semibold"
+      on:click={() => goto("/login")}
+    >
+      Login
+    </button>
   </div>
-  <div class="text-2xl text-white">{signUp ? "Sign Up" : "Log In"}</div>
-  <button
-    class="flex flex-row space-x-2 px-6 py-4 m-4 bg-white rounded-md items-center"
-    on:click={handleGoogleSignIn}
-  >
-    <DeviconGoogle style="font-size: 1.5em"></DeviconGoogle>
-    <div class="font-semibold">Continue with Google</div>
-  </button>
-  <div class="text-xl text-white">Or login with an email:</div>
-  <form class="flex flex-col space-y-2 items-center w-full max-w-md">
-    <input
-      bind:value={email}
-      class="p-2 rounded-lg w-full"
-      type="text"
-      placeholder="Email"
-    />
-    <input
-      bind:value={password}
-      class="p-2 rounded-lg w-full"
-      type="password"
-      placeholder="Password"
-    />
 
-    {#if signUp}
+  <div
+    class="flex-grow h-full text-center justify-center items-center space-y-2 p-4"
+  >
+    <div class="text-center text-4xl font-bold text-white">
+      Howdy there, Welcome to VidScripts
+    </div>
+    <div class="text-2xl text-white">{signUp ? "Sign Up" : "Log In"}</div>
+    <div class="flex justify-center w-full">
+      <button
+        class="flex flex-row space-x-2 px-6 py-4 bg-white rounded-md items-center"
+        on:click={handleGoogleSignIn}
+      >
+        <DeviconGoogle style="font-size: 1.5em"></DeviconGoogle>
+        <div class="font-semibold">Continue with Google</div>
+      </button>
+    </div>
+    <div class="text-xl text-white">Or login with an email:</div>
+    <form class="flex flex-col space-y-2 items-center w-full max-w-md">
       <input
-        bind:value={confirmPassword}
+        bind:value={email}
+        class="p-2 rounded-lg w-full"
+        type="text"
+        placeholder="Email"
+      />
+      <input
+        bind:value={password}
         class="p-2 rounded-lg w-full"
         type="password"
-        placeholder="Confirm Password"
+        placeholder="Password"
       />
-    {/if}
 
-    <button
-      on:click|preventDefault={handleSubmit}
-      class="bg-blue-600 text-white p-2 rounded-lg w-full"
-      type="submit"
-    >
       {#if signUp}
-        Sign Up
-      {:else}
-        Log In
+        <input
+          bind:value={confirmPassword}
+          class="p-2 rounded-lg w-full"
+          type="password"
+          placeholder="Confirm Password"
+        />
       {/if}
-    </button>
 
-    {#if signUp}
-      <div class="flex flex-col items-center text-white">
-        <button
-          class="bg-yellow-500 px-4 py-2 rounded-lg"
-          on:click={() => {
-            signUp = false;
-          }}
-        >
-          Log In
-        </button>
-      </div>
-    {:else}
-      <div class="flex flex-col text-center items-center text-white">
-        <div class="text-md">Don't have an account?</div>
-        <button
-          class="bg-yellow-500 px-4 py-2 rounded-lg"
-          on:click={() => {
-            signUp = true;
-          }}
-        >
+      <button
+        on:click|preventDefault={handleSubmit}
+        class="bg-blue-600 text-white p-2 rounded-lg w-full"
+        type="submit"
+      >
+        {#if signUp}
           Sign Up
-        </button>
-      </div>
-    {/if}
-  </form>
+        {:else}
+          Log In
+        {/if}
+      </button>
+
+      {#if signUp}
+        <div class="flex flex-col items-center text-white">
+          <button
+            class="bg-yellow-500 px-4 py-2 rounded-lg"
+            on:click={() => {
+              signUp = false;
+            }}
+          >
+            Log In
+          </button>
+        </div>
+      {:else}
+        <div class="flex flex-col text-center items-center text-white">
+          <div class="text-md">Don't have an account?</div>
+          <button
+            class="bg-yellow-500 px-4 py-2 rounded-lg"
+            on:click={() => {
+              signUp = true;
+            }}
+          >
+            Sign Up
+          </button>
+        </div>
+      {/if}
+    </form>
+  </div>
 </div>
