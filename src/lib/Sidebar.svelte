53dacab7eec9194dd352048a02c184a37e716ab6
ag0711 <ag0711@tamu.edu>
2024-09-24 22:13:20 -0500

    Script tab is now only shown if a script is selected

--- src/lib/Sidebar.svelte
+++ src/lib/Sidebar.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { goto } from "$app/navigation";
   import { authHandlers } from "./stores/authStore";
+  import { scriptMetaIdStore } from "./stores/scriptStore";
 </script>
 
 <div class="flex flex-col h-full w-64 text-xl text-white bg-[#1f1f1f]">
@@ -10,9 +11,12 @@
   <button class="h-16 hover:bg-gray-600" on:click={() => goto("/brainstorm")}
     >Brainstorm</button
   >
-  <button class="h-16 hover:bg-gray-600" on:click={() => goto("/script")}
-    >Script</button
-  >
+  {#if $scriptMetaIdStore}
+    <!-- Only show this tab if a script has been opened -->
+    <button class="h-16 hover:bg-gray-600" on:click={() => goto("/script")}
+      >Script</button
+    >
+  {/if}
   <button class="h-16 hover:bg-gray-600" on:click={() => goto("/summarize")}
     >Summarize</button
   >
