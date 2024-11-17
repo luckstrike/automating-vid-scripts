4ad9b22614868f01a2b12b196848dde06f0b6516
luckstrike <alexisgarc711@gmail.com>
2024-11-16 23:24:40 -0600

    Forgot to remove the Login page reference in a file (that file was removed)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,6 @@
 <script lang="ts">
-	import Sidebar from '$lib/Sidebar.svelte';
-	import Login from '$lib/Login.svelte';
-	import { authStore } from '$lib/stores/authStore';
+  import Sidebar from "$lib/Sidebar.svelte";
+  import { authStore } from "$lib/stores/authStore";
 </script>
 
-<div class="flex h-screen w-screen">
-	{#if !$authStore.isLoading && !$authStore.currentUser}
-		<Login />
-	{/if}
-</div>
+<div class="flex h-screen w-screen"></div>
