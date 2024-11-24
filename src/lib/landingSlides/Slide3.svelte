c84e36793f3a4b46d8460e5e2f453be23e278333
ag0711 <ag0711@tamu.edu>
2024-11-23 22:33:05 -0600

    Switched the landing pages over to using screenshots instead

--- src/lib/landingSlides/Slide3.svelte
+++ src/lib/landingSlides/Slide3.svelte
@@ -18,7 +18,7 @@
     class="flex flex-col lg:max-w-[45%] text-white items-center space-y-2 align-middle justify-center"
   >
     <div>
-      <video src="summarize.mp4" muted autoplay loop></video>
+      <img src="summarize.png" alt="A screenshot of the Summarize tab" />
     </div>
   </div>
 </div>
