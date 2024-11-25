88dcc6677357a26b5626a3beee325505b561a56d
ag0711 <ag0711@tamu.edu>
2024-11-24 22:48:25 -0600

    Added in an error page for auth + changed returning to throwing for auth logins (see if that fixes the login issue)

---
+++ src/routes/auth/error/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  export let data;
+</script>
+
+<div>
+  <h1>Authentication Error</h1>
+  <p>There was a problem signing you in. Please try again.</p>
+  <a href="/">Return to home</a>
+</div>
